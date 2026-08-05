2c37c38466195202ddfa2d3ffaca24762a05e443
Officemoderneducation <office.moderneducation@gmail.com>
2026-08-05 15:18:59 +0530

    Update firebase-config.js

--- assets/js/firebase-config.js
+++ assets/js/firebase-config.js
@@ -1,23 +1,27 @@
-<script type="module">
-  // Import the functions you need from the SDKs you need
-  import { initializeApp } from "https://www.gstatic.com/firebasejs/12.17.1/firebase-app.js";
-  import { getAnalytics } from "https://www.gstatic.com/firebasejs/12.17.1/firebase-analytics.js";
-  // TODO: Add SDKs for Firebase products that you want to use
-  // https://firebase.google.com/docs/web/setup#available-libraries
+import { initializeApp } 
+from "https://www.gstatic.com/firebasejs/12.17.1/firebase-app.js";
 
-  // Your web app's Firebase configuration
-  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-  const firebaseConfig = {
-    apiKey: "AIzaSyCj3BBjywNWl4ScDJUyrmslg4bHrlMiu_Q",
-    authDomain: "telethoon.firebaseapp.com",
-    projectId: "telethoon",
-    storageBucket: "telethoon.firebasestorage.app",
-    messagingSenderId: "853450341855",
-    appId: "1:853450341855:web:356f9ec0e9a6f88c75d86a",
-    measurementId: "G-EWP905EGQ1"
-  };
+import { getAuth } 
+from "https://www.gstatic.com/firebasejs/12.17.1/firebase-auth.js";
 
-  // Initialize Firebase
-  const app = initializeApp(firebaseConfig);
-  const analytics = getAnalytics(app);
-</script>
+import { getFirestore } 
+from "https://www.gstatic.com/firebasejs/12.17.1/firebase-firestore.js";
+
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCj3BBjywNWl4ScDJUyrmslg4bHrlMiu_Q",
+  authDomain: "telethoon.firebaseapp.com",
+  projectId: "telethoon",
+  storageBucket: "telethoon.firebasestorage.app",
+  messagingSenderId: "853450341855",
+  appId: "1:853450341855:web:356f9ec0e9a6f88c75d86a",
+  measurementId: "G-EWP905EGQ1"
+};
+
+
+const app = initializeApp(firebaseConfig);
+
+
+export const auth = getAuth(app);
+
+export const db = getFirestore(app);
