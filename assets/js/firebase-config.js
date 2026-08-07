1602d8f28f8e3f49ab39ea41b76e87ca98956b26
Officemoderneducation <office.moderneducation@gmail.com>
2026-08-07 10:45:31 +0530

    Update firebase-config.js

--- assets/js/firebase-config.js
+++ assets/js/firebase-config.js
@@ -1,27 +1,24 @@
-import { initializeApp } 
-from "https://www.gstatic.com/firebasejs/12.17.1/firebase-app.js";
+// ======================================
+// Firebase Configuration
+// ======================================
 
-import { getAuth } 
-from "https://www.gstatic.com/firebasejs/12.17.1/firebase-auth.js";
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.17.1/firebase-app.js";
 
-import { getFirestore } 
-from "https://www.gstatic.com/firebasejs/12.17.1/firebase-firestore.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/12.17.1/firebase-auth.js";
 
+import { getFirestore } from "https://www.gstatic.com/firebasejs/12.17.1/firebase-firestore.js";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCj3BBjywNWl4ScDJUyrmslg4bHrlMiu_Q",
-  authDomain: "telethoon.firebaseapp.com",
-  projectId: "telethoon",
-  storageBucket: "telethoon.firebasestorage.app",
-  messagingSenderId: "853450341855",
-  appId: "1:853450341855:web:356f9ec0e9a6f88c75d86a",
-  measurementId: "G-EWP905EGQ1"
+    apiKey: "AIzaSyCj3BBjywNWl4ScDJUyrmslg4bHrlMiu_Q",
+    authDomain: "telethoon.firebaseapp.com",
+    projectId: "telethoon",
+    storageBucket: "telethoon.firebasestorage.app",
+    messagingSenderId: "853450341855",
+    appId: "1:853450341855:web:356f9ec0e9a6f88c75d86a",
+    measurementId: "G-EWP905EGQ1"
 };
-
 
 const app = initializeApp(firebaseConfig);
 
-
 export const auth = getAuth(app);
-
 export const db = getFirestore(app);
